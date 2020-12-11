0cab40c560e76825bd04c37979006c644d83fcd3
shiyingt <teyshiying1999@gmail.com>
2020-12-11 15:44:16 +0800

    cleanup

--- flask/app.py
+++ flask/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify,json
 import requests
 from flask_cors import CORS
 
-headers ={'x-api-key':'zBrlPqfHv28k7P4gr47sk3TM3xF3uE8WdHHHl6kh'}
+header = {'x-api-key': 'zBrlPqfHv28k7P4gr47sk3TM3xF3uE8WdHHHl6kh', 'Content-Type': 'application/json'}
 app = Flask(__name__)
 CORS(app)
 
@@ -16,7 +16,7 @@ def getTransaction(acc_id):
     response = requests.post(
         'https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/transaction/view',
         json={'custID':acc_id},
-        headers={'x-api-key': 'zBrlPqfHv28k7P4gr47sk3TM3xF3uE8WdHHHl6kh', 'Content-Type': 'application/json'},
+        headers=header,
     )
     json_response = jsonify(response.json())
     json_obj = response.json()
@@ -30,7 +30,7 @@ def getAccount(acc_id):
     responseCustomer = requests.post(
         'https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/accounts/view',
         json={'custID':acc_id},
-        headers={'x-api-key': 'zBrlPqfHv28k7P4gr47sk3TM3xF3uE8WdHHHl6kh', 'Content-Type': 'application/json'},
+        headers=header,
     )
     return jsonify(responseCustomer.json())
 
@@ -38,12 +38,10 @@ def getAccount(acc_id):
 def getAll():
     response = requests.post(
         'https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/users',
-        headers={'x-api-key': 'zBrlPqfHv28k7P4gr47sk3TM3xF3uE8WdHHHl6kh','Content-Type': 'application/json'},
+        headers=header,
     )
     status_code = response.status_code
-    print(status_code)
     if status_code == 200:
-        print(response.json())
         return jsonify(response.json())
     else:
         print('response is not 200')
@@ -54,11 +52,8 @@ def updateAccount(acc_id,amount):
     response = requests.post(
         'https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/accounts/update',
         json={'custID':acc_id, 'amount':amount},
-        headers={'x-api-key': 'zBrlPqfHv28k7P4gr47sk3TM3xF3uE8WdHHHl6kh', 'Content-Type': 'application/text'},
+        headers=header,
     )
-    print(response.status_code)
-    print(response.text)
-    # json_response = jsonify(response.json())
     return response.text
 
 
