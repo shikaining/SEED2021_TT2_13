35e18f23052922a984ef07a94c0bd3600c56ee9f
Ang Li Min <anglimin@users.noreply.github.com>
2020-12-11 15:45:29 +0800

    added somethings to the card

--- src/components/TransactionCard.js
+++ src/components/TransactionCard.js
@@ -1,24 +1,44 @@
-import React, {useState} from 'react'
+import React, {Component} from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
 
-
-
-function TransactionCard() {
-    const [expanded,setExpanded] = useState(false)
-
-    const handleExpandClick = () => {
-        setExpanded(!expanded)
+const Styles = makeStyles((theme) => ({
+    root:{
+        maxWidth:345
     }
+}))
 
-    return(
-        <Card>
-            <CardContent>
-                
-            </CardContent>
-        </Card>
-    )
+class TransactionCard extends Component {
+    
+    render(){
+        const {amount, name, date,category,eGift} = this.props
+        return(
+            <Card className={classes.root}>
+                <CardContent>
+                    <Typography variant="body2">
+                        {name}
+                    </Typography>
+                </CardContent>
+                <CardContent>
+                    <Typography variant="body2">
+                        {amount}
+                    </Typography>
+                </CardContent>
+                <CardContent>
+                    <Typography variant="body2">
+                        {category}
+                    </Typography>
+                </CardContent>
+                <CardContent>
+                    <Typography variant="body2">
+                        {date}
+                    </Typography>
+                </CardContent>
+            </Card>
+        )
+    }
 }
 
-export default TransactionCard
+export default withStyles(Styles)(TransactionCard)
